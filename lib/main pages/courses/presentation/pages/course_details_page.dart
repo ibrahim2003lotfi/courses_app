1eeb3988c640e1f5496552c188ef45d65ee659c5
ibrahim2003lotfi <ibrahim2003lotfi@gmail.com>
2025-09-22 14:11:58 +0300

    New Navigation

---
+++ lib/main pages/courses/presentation/pages/course_details_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CourseDetailsPage extends StatelessWidget {
+  const CourseDetailsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Courses Page')),
+      body: Center(child: Text('Courses Page')),
+    );
+  }
+}
--- lib/widget_tree.dart
+++ lib/widget_tree.dart
@@ -1,10 +1,12 @@
 import 'package:courses_app/data/notifiers.dart';
+import 'package:courses_app/main%20pages/courses/presentation/pages/course_details_page.dart';
 import 'package:courses_app/main%20pages/home/presentation/pages/home_page.dart';
 import 'package:courses_app/main%20pages/profile/presentation/pages/profile_page.dart';
+import 'package:courses_app/main%20pages/search/presentation/pages/search_page.dart';
 import 'package:courses_app/navbar/presentation/main_navbar_page.dart';
 import 'package:flutter/material.dart';
 
-List<Widget> pages = [HomePage(), ProfilePage()];
+List<Widget> pages = [HomePage(), SearchPage(), CourseDetailsPage(), ProfilePage()];
 
 class WidgetTree extends StatelessWidget {
   const WidgetTree({super.key});
