76b09ee4fbbde669c8a32bc0d1774e289d10e2fd
ibrahim2003lotfi <ibrahim2003lotfi@gmail.com>
2025-09-22 13:38:53 +0300

    Navbar create

---
+++ lib/navbar/presentation/main_navbar_page.dart
@@ -0,0 +1,41 @@
+import 'package:courses_app/data/notifiers.dart';
+import 'package:flutter/material.dart';
+
+class NavBarWidget extends StatefulWidget {
+  const NavBarWidget({super.key});
+
+  @override
+  State<NavBarWidget> createState() => _NavBarWidgetState();
+}
+
+class _NavBarWidgetState extends State<NavBarWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: selectedPageNotifier,
+      builder: (BuildContext context, dynamic selectedpage, Widget? child) {
+        return NavigationBar(
+          destinations: [
+            NavigationDestination(
+              icon: Icon(Icons.home_rounded),
+              label: 'Home',
+            ),
+            NavigationDestination(icon: Icon(Icons.search), label: 'Search'),
+            NavigationDestination(
+              icon: Icon(Icons.local_offer_outlined),
+              label: 'Courses',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.receipt_long_rounded),
+              label: 'Profile',
+            ),
+          ],
+          onDestinationSelected: (int value) {
+            selectedPageNotifier.value = value;
+          },
+          selectedIndex: selectedpage,
+        );
+      },
+    );
+  }
+}
